27a37a3db8efaadfa359b10a669592ba9c2a0ca1
Wesley Gerelle <wesley.gerelle@gmail.com>
2024-11-19 09:51:25 -0800

    Switched to single quotes

--- scripts/0a_filter-species.sh
+++ scripts/0a_filter-species.sh
@@ -1,3 +1,3 @@
 #!/bin/zsh
 
-parallel "cd {//} && python3 ../../scripts/0a_filter-species.py {/} taxa_*.txt" ::: ../alignments/*/*.fasta
+parallel 'cd {//} && python3 ../../scripts/0a_filter-species.py {/} taxa_*.txt' ::: ../alignments/*/*.fasta
