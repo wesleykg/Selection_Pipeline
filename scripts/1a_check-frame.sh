f63fff9859c2ce385a6f9896a3c3818159c6ed46
Wesley Gerelle <wesley.gerelle@gmail.com>
2024-11-20 11:51:44 -0800

    Updated error handling

--- scripts/1a_check-frame.sh
+++ scripts/1a_check-frame.sh
@@ -1,5 +1,12 @@
 #!/bin/zsh
 
-rm -f ../alignments/*/issues.txt
-parallel "cd {//} && sed 's/-/N/g' {/} > temp_{/} && python3 ../../scripts/1a_check-frame.py temp_{/} >> issues.txt && rm temp_{/}" ::: ../alignments/*/*.fasta
-parallel "cd {} && [ -f issues.txt ] && sort -u issues.txt -o issues.txt" ::: ../ ../alignments/*/
+setopt +o nomatch  # Adjust globbing behavior
+
+# Remove old issues.txt files, and doesn't throw an error if it doesn't exist yet
+rm -f ../alignments/*/issues.txt(N)
+
+# Run the check_frame.py script in Parallel
+parallel 'cd {//} && python3 ../../scripts/1a_check-frame.py {/} >> issues.txt' ::: ../alignments/*/*.fasta
+
+# Sort and remove duplicates from the issues.txt files
+parallel 'cd {} && if [ -f issues.txt ]; then sort -u issues.txt -o issues.txt; fi' ::: ../ ../alignments/*/
