44b152405111ff8effc63cf8b368eb16170f78b9
Wesley Gerelle <wesley.gerelle@gmail.com>
2024-11-20 10:24:58 -0800

    Removed erroneous character

--- scripts/1_trim-terminal-stops.sh
+++ scripts/1_trim-terminal-stops.sh
@@ -1,3 +1,3 @@
 #!/bin/zsh
 
-parallel 'cd {//} && python3 ..//../scripts/1_trim-terminal-stops.py {/}' ::: ../alignments/*/*.fasta
+parallel 'cd {//} && python3 ../../scripts/1_trim-terminal-stops.py {/}' ::: ../alignments/*/*.fasta
